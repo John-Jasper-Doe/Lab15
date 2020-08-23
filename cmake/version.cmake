4b534fcc87cac07235b9b08b7d1567368b03d9af
John Doe <john.jasper.doe@gmail.com>
2020-08-24 00:03:44 +0300

    Commit for documentation

--- cmake/version.cmake
+++
@@ -1,7 +0,0 @@
-# For generated new version of this project, Update the version numbers below.
-# This makefile will generate a new version.hpp, *AMEND THE MOST RECENT
-# COMMIT*, and git-tag the commit.
-
-set(KKMEANS_MAJOR 0)
-set(KKMEANS_MINOR 0)
-set(KKMEANS_PATCH 1)
--- test_data/run_rnd_dat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat rnd.txt | ./kkmeans -k 3 > rnd_out.csv
-gnuplot -e "set title 'OutData';set ylabel 'Y-axis';set xlabel 'X-axis';set grid;set term png size 1024,960;set output 'rnd_result.png';set datafile separator ';';set palette model RGB defined (0 'red',1 'blue', 2 'green', 3 'black', 4 'yellow', 5 'brown');plot 'rnd_out.csv' using 1:2:3 notitle with points pt 7 palette"
-
